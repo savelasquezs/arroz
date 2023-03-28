d4c0bfc353efcf84cee1bf213fc2b523be86f2bc
Santiago Velásquez <santyvano@outlook.com>
2023-03-28 16:45:12 -0500

    refactor: :art: Se extrapolo codigo que se utilizaba en varias partes como el boton de agregar(pedido, venta, producto, gasto)

---
+++ src/components/gastoForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="offcanvas offcanvas-end"
+    tabindex="-1"
+    id="offcanvasTop"
+    aria-labelledby="offcanvasTopLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasTopLabel">Nuevo Gasto</h5>
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingInput"
+          placeholder="Nombre Gasto"
+        />
+        <label for="floatingInput">Nombre Gasto</label>
+      </div>
+      <div class="form-floating">
+        <select
+          class="form-select"
+          id="floatingSelect"
+          aria-label="Floating label select example"
+        >
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <label for="floatingSelect">categoria</label>
+      </div>
+
+      <div class="form-floating">
+        <textarea
+          class="form-control"
+          placeholder="anotaciones"
+          id="floatingTextarea"
+        ></textarea>
+        <label for="floatingTextarea">Anotaciones</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Gastos.vue
@@ -0,0 +1,31 @@
+<template>
+  <gasto-form />
+  <div class="container gastosCont">
+    <h2>Tabla de Gastos</h2>
+    <hr />
+    <div class="input-group mb-3 d-flex justify-content-end">
+      <button-add
+        mensaje="Nuevo Gasto"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasTop"
+        aria-controls="offcanvasTop"
+      />
+    </div>
+  </div>
+  <h3></h3>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+import GastoForm from "../components/gastoForm.vue";
+import buttonAdd from "../components/buttonAdd.vue";
+export default {
+  components: { Icon, GastoForm, buttonAdd },
+};
+</script>
+
+<style scoped>
+.gastosCont {
+  padding-top: 100px;
+}
+</style>
