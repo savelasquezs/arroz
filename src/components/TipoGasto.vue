a1a5ab0f35826a39485f8e82d49af40424a6dcba
Santiago Velásquez <santyvano@outlook.com>
2023-04-08 09:52:57 -0500

    feat: :sparkles: Se crea la primer parte de el crud de tipos de gastos

---
+++ src/components/TipoGasto.vue
@@ -0,0 +1,31 @@
+<template>
+  <cat-container-tipo-gasto
+    v-for="gasto in allTipoGastos"
+    :key="gasto.docId"
+    :gasto="gasto"
+  />
+</template>
+
+<script>
+import { mapState } from "pinia";
+import { useTipoGastos } from "../store/gastos";
+import CatContainerTipoGasto from "./CatContainerTipoGasto.vue";
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(useTipoGastos, ["allTipoGastos"]),
+  },
+
+  components: {
+    CatContainerTipoGasto,
+  },
+};
+</script>
+
+<style scoped>
+.cat-container {
+  width: 100px;
+}
+</style>
