dbe519b31c7b597842b47b564c0353681b9fca71
Santiago Velásquez <santyvano@outlook.com>
2023-04-22 08:27:18 -0500

    feat: :sparkles: Creación inicial de componente tabs copy, falta probar

--- src/views/VIstaDomiciliarios.vue
+++ src/views/VIstaDomiciliarios.vue
@@ -1,16 +1,38 @@
 <template>
-  <div>Domiciliarios al ataque</div>
-  <button @click="cerrarSesion" class="btn btn-danger">Cerrar Sesion</button>
+  <div class="container d-flex justify-content-between mt-3">
+    <div>Domiciliarios al ataque</div>
+    <div class="d-flex justify-content-between">
+      <h4 class="mx-3 my-0">
+        {{ usuario }}
+      </h4>
+      <button @click="cerrarSesion" class="btn btn-outline-danger">
+        Cerrar Sesion
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from "pinia";
 import { useAuth } from "../store/auth";
+import { usePedidosStore } from "../store/main";
 
 export default {
   methods: {
     cerrarSesion() {
       useAuth().logout();
     },
+  },
+  computed: {
+    usuario() {
+      return localStorage.getItem("user")
+        ? JSON.parse(localStorage.getItem("user"))
+            .displayName.split(" ")
+            .map((word) => word[0].toUpperCase() + word.substring(1))
+            .join(" ")
+        : "";
+    },
+    ...mapState(usePedidosStore, ["pedidosDatabase"]),
   },
 };
 </script>
