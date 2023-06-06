8e41a8226b188978dd3d71c2df72dcdd27c04666
Santiago Velásquez <santyvano@outlook.com>
2023-06-06 17:08:21 -0500

    refactor: :art: se mejora el componente de PedidosForm para que sea reutilizable

---
+++ src/views/Config.vue
@@ -0,0 +1,14 @@
+<template>
+  <navbar class="position-fixed" />
+  <div class="contenido"></div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: { Navbar },
+};
+</script>
+
+<style scoped>
+</style>
